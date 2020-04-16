b6eae9a66ec7921319ce1732f7f723fa73e0de46
Samikshya Regmi <sraj@Samikshyas-MacBook-Air.local>
2020-04-16 08:47:54 +0545

    change status of the stocks

--- resources/views/backend/stock/index.blade.php
+++ resources/views/backend/stock/index.blade.php
@@ -92,108 +92,108 @@
 
 @endsection
 @section('js_script')
-    <script>
-        $(document).ready( function () {
+    {{--<script>--}}
+        {{--$(document).ready( function () {--}}
 
-            $('#table').on('click','.delete-content',function(event){
-                event.preventDefault();
-                $object = $(this);
-                var id  = $(this).attr('data-type');
-                var url = baseUrl+"/stocks/"+id;
-                swal({
-                    title: 'Are you sure?',
-                    text: 'You will not be able to recover this !',
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonText: 'Yes, delete it!',
-                    cancelButtonText: 'No, keep it'
-                }).then((result) => {
-                    if (result.value
-                    )
-                    {
+            {{--$('#table').on('click','.delete-content',function(event){--}}
+                {{--event.preventDefault();--}}
+                {{--$object = $(this);--}}
+                {{--var id  = $(this).attr('data-type');--}}
+                {{--var url = baseUrl+"/stocks/"+id;--}}
+                {{--swal({--}}
+                    {{--title: 'Are you sure?',--}}
+                    {{--text: 'You will not be able to recover this !',--}}
+                    {{--type: 'warning',--}}
+                    {{--showCancelButton: true,--}}
+                    {{--confirmButtonText: 'Yes, delete it!',--}}
+                    {{--cancelButtonText: 'No, keep it'--}}
+                {{--}).then((result) => {--}}
+                    {{--if (result.value--}}
+                    {{--)--}}
+                    {{--{--}}
 
-                        $.ajax({
-                            type: "Delete",
-                            url: url,
-                            data: {
-                                id: id,
-                                _method: 'DELETE'
-                            },
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                            },
-                            success: function (response) {
-                                swal("Deleted!", response.message, "success");
+                        {{--$.ajax({--}}
+                            {{--type: "Delete",--}}
+                            {{--url: url,--}}
+                            {{--data: {--}}
+                                {{--id: id,--}}
+                                {{--_method: 'DELETE'--}}
+                            {{--},--}}
+                            {{--headers: {--}}
+                                {{--'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
+                            {{--},--}}
+                            {{--success: function (response) {--}}
+                                {{--swal("Deleted!", response.message, "success");--}}
 
-                                var nRow = $($object).parents('tr')[0];
-                                nRow.remove();
-                            },
-                            error: function (e) {
-                                if (e.responseJSON.message) {
-                                    swal('Error', e.responseJSON.message, 'error');
-                                } else {
-                                    swal('Error', 'Something went wrong while processing your request.', 'error')
-                                }
-                            }
-                        });
-                    }
-                })
-            })
-
-
-            $("#table").on("click", "#change-status", function () {
-                $object = $(this);
-                var id = $(this).attr('data-type');
-                swal({
-                    title: 'Are you sure?',
-                    text: 'Do you want to change the status',
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonText: 'Yes, change it!',
-                    cancelButtonText: 'No, keep it'
-                }).then((result) => {
-                    if (result.value) {
-                        $.ajax({
-                            type: "POST",
-                            url: "{{ route('stocks.change-status') }}",
-                            data: {
-                                'id': id,
-                            },
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                            },
-                            dataType: 'json',
-                            success: function (response) {
-                                swal("Thank You!", response.message, "success");
-                                if (response.stocks.is_active == 1) {
-                                    $($object).children().removeClass('fa fa-minus');
-                                    $($object).children().addClass('fa fa-check');
-                                } else {
-                                    $($object).find('.unpublished').html('<i class="fa fa-minus" aria-hidden="true"></i>');
-                                    $($object).children().removeClass('fa fa-check');
-                                    $($object).children().addClass('fa fa-minus');
-                                }
-                            },
-                            error: function (e) {
-                                if (e.responseJSON.message) {
-                                    swal('Error', e.responseJSON.message, 'error');
-                                } else {
-                                    swal('Error', 'Something went wrong while processing your request.', 'error')
-                                }
-                            }
-                        });
-
-                    }
-                })
-            });
+                                {{--var nRow = $($object).parents('tr')[0];--}}
+                                {{--nRow.remove();--}}
+                            {{--},--}}
+                            {{--error: function (e) {--}}
+                                {{--if (e.responseJSON.message) {--}}
+                                    {{--swal('Error', e.responseJSON.message, 'error');--}}
+                                {{--} else {--}}
+                                    {{--swal('Error', 'Something went wrong while processing your request.', 'error')--}}
+                                {{--}--}}
+                            {{--}--}}
+                        {{--});--}}
+                    {{--}--}}
+                {{--})--}}
+            {{--})--}}
 
 
+            {{--$("#table").on("click", "#change-status", function () {--}}
+                {{--$object = $(this);--}}
+                {{--var id = $(this).attr('data-type');--}}
+                {{--swal({--}}
+                    {{--title: 'Are you sure?',--}}
+                    {{--text: 'Do you want to change the status',--}}
+                    {{--type: 'warning',--}}
+                    {{--showCancelButton: true,--}}
+                    {{--confirmButtonText: 'Yes, change it!',--}}
+                    {{--cancelButtonText: 'No, keep it'--}}
+                {{--}).then((result) => {--}}
+                    {{--if (result.value) {--}}
+                        {{--$.ajax({--}}
+                            {{--type: "POST",--}}
+                            {{--url: "{{ route('stocks.change-status') }}",--}}
+                            {{--data: {--}}
+                                {{--'id': id,--}}
+                            {{--},--}}
+                            {{--headers: {--}}
+                                {{--'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
+                            {{--},--}}
+                            {{--dataType: 'json',--}}
+                            {{--success: function (response) {--}}
+                                {{--swal("Thank You!", response.message, "success");--}}
+                                {{--if (response.stocks.is_active == 1) {--}}
+                                    {{--$($object).children().removeClass('fa fa-minus');--}}
+                                    {{--$($object).children().addClass('fa fa-check');--}}
+                                {{--} else {--}}
+                                    {{--$($object).find('.unpublished').html('<i class="fa fa-minus" aria-hidden="true"></i>');--}}
+                                    {{--$($object).children().removeClass('fa fa-check');--}}
+                                    {{--$($object).children().addClass('fa fa-minus');--}}
+                                {{--}--}}
+                            {{--},--}}
+                            {{--error: function (e) {--}}
+                                {{--// if (e.responseJSON.message) {--}}
+                                {{--//     swal('Error', e.responseJSON.message, 'error');--}}
+                                {{--// } else {--}}
+                                {{--//     swal('Error', 'Something went wrong while processing your request.', 'error')--}}
+                                {{--// }--}}
+                            {{--}--}}
+                        {{--});--}}
+
+                    {{--}--}}
+                {{--})--}}
+            {{--});--}}
 
 
 
 
 
-        });
 
-    </script>
+
+        {{--});--}}
+
+    {{--</script>--}}
 @endsection
